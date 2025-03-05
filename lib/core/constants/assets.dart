9953eeaff07fe4fdc6be8e120b7fa8e8cd65d6e4
Wojuade Abdul Afeez <wojucrown7@gmail.com>
2025-03-05 16:29:59 +0100

    Add SVG assets for selection and update ImageAssets class

--- lib/core/constants/assets.dart
+++ lib/core/constants/assets.dart
@@ -6,4 +6,10 @@ class ImageAssets {
 
   // Images
   static const String user = '$_imageAssetPath/person.png';
+
+  // SVGs
+
+  static const String select1 = '$_svgAssetPath/select_1.svg';
+  static const String select2 = '$_svgAssetPath/select_2.svg';
+  static const String select3 = '$_svgAssetPath/select_3.svg';
 }
