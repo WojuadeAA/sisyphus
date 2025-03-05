eff1595a2c8bef07d64241ab6106c00fdcf09785
Wojuade Abdul Afeez <wojucrown7@gmail.com>
2025-03-05 16:31:25 +0100

    Add new SVG assets for globe, menu, logos, charts, and BTC/USD pair; update ImageAssets class

--- lib/core/constants/assets.dart
+++ lib/core/constants/assets.dart
@@ -12,4 +12,12 @@ class ImageAssets {
   static const String select1 = '$_svgAssetPath/select_1.svg';
   static const String select2 = '$_svgAssetPath/select_2.svg';
   static const String select3 = '$_svgAssetPath/select_3.svg';
+  static const String globe = '$_svgAssetPath/globe.svg';
+  static const String menu = '$_svgAssetPath/menu.svg';
+  static const String logoLight = '$_svgAssetPath/logo_light.svg';
+  static const String logoDark = '$_svgAssetPath/logo_dark.svg';
+  static const String btcUsdPair = '$_svgAssetPath/btc_usd_pair.svg';
+  static const String charts = '$_svgAssetPath/chart.svg';
+  static const String expand = '$_svgAssetPath/expand.svg';
+  static const String arrowDown = '$_svgAssetPath/arrow_down.svg';
 }
