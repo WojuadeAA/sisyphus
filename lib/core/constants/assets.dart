4e562ae3722506bb5215a60d7821f43e1b07a7d1
Wojuade Abdul Afeez <wojucrown7@gmail.com>
2025-03-05 16:29:09 +0100

    Add person image asset and create ImageAssets class for asset management

---
+++ lib/core/constants/assets.dart
@@ -0,0 +1,9 @@
+class ImageAssets {
+  ImageAssets._();
+
+  static const String _imageAssetPath = 'assets/images';
+  static const String _svgAssetPath = 'assets/svgs';
+
+  // Images
+  static const String user = '$_imageAssetPath/person.png';
+}
