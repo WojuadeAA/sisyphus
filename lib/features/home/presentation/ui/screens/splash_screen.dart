bba3c7399b2420fe95e2bd58d61e4ae1a2b5430a
Wojuade Abdul Afeez <wojucrown7@gmail.com>
2025-03-06 04:32:52 +0100

    Add SplashScreen widget with navigation to HomeScreen

---
+++ lib/features/home/presentation/ui/screens/splash_screen.dart
@@ -0,0 +1,44 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:sisyphus/core/core.dart';
+import 'package:sisyphus/features/home/presentation/ui/screens/home_screen.dart';
+import 'package:sisyphus/shared/utils/utils.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  startTimeout() => Timer(const Duration(seconds: 2), changeScreen);
+
+  Future<void> changeScreen() async {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const HomeScreen(),
+      ),
+    );
+    //Show status bar
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: SystemUiOverlay.values);
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    startTimeout();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(color: Theme.of(context).canvasColor),
+        child: Center(
+          child: SvgPicture.asset(context.isDarkMode() ? ImageAssets.logoLight : ImageAssets.logoDark),
+        ));
+  }
+}
