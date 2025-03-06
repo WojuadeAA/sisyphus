01174e767d834812c364c8135d6da13b4c3f9695
Wojuade Abdul Afeez <wojucrown7@gmail.com>
2025-03-06 05:09:45 +0100

    Add bottom sheet utility method to ThemeExtension for improved UI interactions

--- lib/shared/utils/extensions.dart
+++ lib/shared/utils/extensions.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:sisyphus/core/core.dart';
 import 'package:sisyphus/shared/utils/utils.dart';
 
 extension ThemeExtension on BuildContext? {
   bool isDarkMode() {
     final brightness = Theme.of(this!).brightness;
     return brightness == Brightness.dark;
+  }
+
+  void openBottomSheet(Widget bottomSheet) {
+    showModalBottomSheet<dynamic>(
+      isScrollControlled: true,
+      context: this!,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(16),
+          topRight: Radius.circular(16),
+        ),
+      ),
+      backgroundColor: isDarkMode() ? const Color(0xff20252B) : white,
+      barrierColor: Colors.black.withOpacity(0.5),
+      builder: (BuildContext buildContext) {
+        return bottomSheet;
+      },
+    );
   }
 }
 
